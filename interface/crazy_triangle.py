d77722b83214ecdb5d6d8cb2eaa51875af34f673
NooBAsTiK <kuda_pishesh@mail.ru>
2024-08-08 20:46:41 +0300

    new figure "crazy triangle"

---
+++ interface/crazy_triangle.py
@@ -0,0 +1,38 @@
+import pygame
+import random
+from interface import interface_object
+
+
+screen = pygame.display.set_mode((200, 200))
+class Triangle(interface_object.InterfaceObject):
+    def __init__(self, position, size, color):
+        super().__init__()
+        self.position = position
+        self.size = size
+        self.color = color
+
+    def move_away_from_mouse(self, mouse_pos):
+        x, y = self.position
+        mx, my = mouse_pos
+
+        # Случайный тремор
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1),
+                      (1, -1), (1, 1)]
+        dx, dy = random.choice(directions)
+
+        # Случайное движение от мышки
+        step_size = random.randint(5, 10)
+        x += dx * step_size
+        y += dy * step_size
+
+        # Запрет на выход границ
+        x = max(0, min(x, screen.get_width() - self.size))
+        y = max(self.size, min(y, screen.get_height()))
+
+        self.position = (x, y)
+
+    def draw(self, screen):
+        x, y = self.position
+        points = [(x, y), (x + self.size, y),
+                  (x + self.size // 2, y - self.size)]
+        pygame.draw.polygon(screen, self.color, points)
--- main.py
+++ main.py
@@ -3,24 +3,26 @@ import random
 from interface.button import Button
 from interface.label import Label
 from interface import interface_object
+from interface.crazy_triangle import Triangle
 
 pygame.init()
 screen = pygame.display.set_mode((200, 200))
 clock = pygame.time.Clock()
 
 running = True
-color = (0, 0, 0)
 
-drawables = []
-clickable = []
-
-button1 = Button((10, 10), (100, 40), (255, 100, 100), func=lambda: print("Я ПЕРВАЯ КНОПКА"))
+button1 = Button((10, 10), (100, 40), (255, 100, 100),
+                 func=lambda: print("Я ПЕРВАЯ КНОПКА"))
 button2 = Button((80, 60), (100, 40), (255, 100, 100))
 
 label = Label(position=(10, 0), text="HELLO WORLD")
 
+triangle = Triangle(position=(100, 100), size=30, color=(0, 0, 255))
+
+
 def button2_func():
-    button2.set_color((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+    button2.set_color((random.randint(0, 255), random.randint(0, 255),
+                       random.randint(0, 255)))
 
 
 button2.set_function(button2_func)
@@ -31,10 +33,12 @@ for i in interface_object.InterfaceObject.get_items():
 while running:
     events = []
     for event in pygame.event.get():
-        if event.type == pygame.WINDOWCLOSE:
+        if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             interface_object.handle_interface_objects_click(event)
+
+    triangle.move_away_from_mouse(pygame.mouse.get_pos())
 
     screen.fill((255, 255, 255))
     interface_object.draw_interface_objects(screen)
